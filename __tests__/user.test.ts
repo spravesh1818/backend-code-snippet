772e9972db8cc7473bbda4c90b83977c998cde43
Pravesh Chapagain <spravesh1818@live.com>
2022-05-16 15:18:40 +0545

    Minor user test bug solved

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -1,19 +1,19 @@
 import { iUserPayload } from "../src/common/dto/user.dto";
 import * as userService from "../src/core/services/user.service";
+
 describe("Testing users", () => {
   test("Get User List", () => {
     const workflows = userService.getUsers();
-
     expect(workflows).toBeInstanceOf([]);
   });
 
   test("Add a workflow", () => {
-    const workflowData: iUserPayload = {
+    const userData: iUserPayload = {
       name: "Pravesh Chapagain",
       email: "spravesh1818@gmail.com",
     };
 
-    const workflowResponse = userService.insertUser(workflowData);
-    expect(workflowResponse).toBeInstanceOf(iWorkflowPayload);
+    const workflowResponse = userService.insertUser(userData);
+    expect(workflowResponse).toBeInstanceOf(iUserPayload);
   });
 });
