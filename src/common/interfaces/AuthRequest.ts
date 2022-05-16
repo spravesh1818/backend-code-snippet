7715c70c53324116ae119e047c026f447a4c7240
Pravesh Chapagain <spravesh1818@live.com>
2022-05-16 19:48:19 +0545

    Minor bug resolved

--- src/common/interfaces/AuthRequest.ts
+++ src/common/interfaces/AuthRequest.ts
@@ -1,6 +1,7 @@
-import { Request } from 'express';
+import User from "../interfaces/User";
+import { Request } from "express";
 
 export interface AuthenticatedRequest extends Request {
-    currentUser?: any; // TODO - remove temporary datatype
-    currentApp?: string;
+  currentUser?: User; // TODO - remove temporary datatype
+  currentApp?: string;
 }
